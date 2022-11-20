9eb097eb1f637db083ab14cd42c8eeff15e878f4
RISU6969 <118008446+RISU6969@users.noreply.github.com>
2022-11-20 12:03:27 +0200

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router  = express.Router();
-const {ensureAuthenticated} = require('../config/auth') 
+const {ensureAuthenticated} = require('./config/auth') 
 //login page
 //register page
 router.get('/test69', (req,res)=>{
-    res.sendFile('./welcome.html', {root: 'views'})
+    res.sendFile('./welcome.html', {root: ''})
 })
 router.get('/register', (req,res)=>{
     res.render('register');
 })
+app.use(express.staticProvider(__dirname + '/views'));
+
+app.get('/commands/slashcommands', function(req, res) {
+    res.render('slashcommands.html');
+});
 router.get('/dashboard',ensureAuthenticated,(req,res)=>{
     res.render(('dashboard'), {
         user: req.user
